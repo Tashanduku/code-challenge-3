24575b82db3736d6a24a73456fdac3d76689c176
Natasha Nduku <natashanduku0@gmail.com>
2024-12-16 17:57:42 +0300

    adds movies

--- movie.js
+++ movie.js
@@ -9,3 +9,23 @@ const filmShowtime = document.querySelector("#showtime");
 const filmDescription = document.querySelector("#description");
 const filmTickets = document.querySelector("#tickets");
 const buyTicketButton = document.querySelector("#buy-ticket");
+
+//fetch and display all movies
+function fetchAllMovies() {
+  fetch(baseURL)
+    .then((response) => response.json())
+    .then((movies) => {
+      filmList.innerHTML = ""; // Clear the placeholder
+      movies.forEach((movie) => {
+        const movieItem = document.createElement("li");
+        movieItem.textContent = movie.title;
+        movieItem.classList.add("film", "item");
+        if (movie.capacity - movie.tickets_sold === 0) {
+          movieItem.classList.add("sold-out");
+        }
+        movieItem.addEventListener("click", () => displayMovieDetails(movie));
+        filmList.appendChild(movieItem);
+      });
+    })
+    .catch((error) => console.error("Error fetching movies:", error));
+}
