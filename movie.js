6ece299f2b058d29024bd765de9323dc020d8281
Natasha Nduku <natashanduku0@gmail.com>
2024-12-16 17:56:48 +0300

    adds the dom elements

---
+++ movie.js
@@ -0,0 +1,11 @@
+const baseURL = "http://localhost:3000/films";
+
+// DOM elements
+const filmList = document.querySelector("#films");
+const filmPoster = document.querySelector("#poster");
+const filmTitle = document.querySelector("#title");
+const filmRuntime = document.querySelector("#runtime");
+const filmShowtime = document.querySelector("#showtime");
+const filmDescription = document.querySelector("#description");
+const filmTickets = document.querySelector("#tickets");
+const buyTicketButton = document.querySelector("#buy-ticket");
